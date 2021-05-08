ca494d877d20d9fdd9c48e67bf33f44cbbbceee5
sagar0708 <sagarchandresha0708@gmail.com>
2021-05-08 21:02:27 +0530

    add footer

--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ $(document).ready(function(){
   $(".verticle-nav .menu-list li a").on("click", function(){
     $(".sidebar").toggleClass("active");
   })
+  $(".testimonials.slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false
+  });
 });
 function myMap() {
   var myCenter = new google.maps.LatLng(21.502609,70.419346)
