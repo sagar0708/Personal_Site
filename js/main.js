22a06358fd03fc6728599f3d7b2709022075c70a
sagar0708 <sagarchandresha0708@gmail.com>
2021-05-08 19:29:23 +0530

    add google map

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ $(document).ready(function(){
     $(".sidebar").toggleClass("active");
   });
 });
+function myMap() {
+  var myCenter = new google.maps.LatLng(21.502609,70.419346)
+  var mapProp= {
+    center:myCenter,
+    zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("map"),mapProp);
+  var marker = new google.maps.Marker({position: myCenter});
+  marker.setMap(map); 
+}
