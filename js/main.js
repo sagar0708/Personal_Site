1b221a3f28d56a7fe1bc16a30d706614cf28d6f1
sagar0708 <sagarchandresha0708@gmail.com>
2021-06-11 12:46:51 +0530

    remove loader

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-function loader(){
-  gsap.from(".body",1,{opacity:0}) 
-  $(".loader").fadeOut(2000);
-}
-// Google Map
-function myMap() {
-  var myCenter = new google.maps.LatLng(21.502609,70.419346)
-  var mapProp= {
-    center:myCenter,
-    zoom:5,
-  };
-  var map = new google.maps.Map(document.getElementById("map"),mapProp);
-  var marker = new google.maps.Marker({position: myCenter});
-  marker.setMap(map); 
-}
 $(document).ready(function(){
   $(".menu-icon").on("click", function(){
     $(".sidebar").toggleClass("active");
